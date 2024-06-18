5a4cac41e35493e2e1fc78ae2e1c93cdb634faaa
Phat Nguyen <phattrong.a@gmail.com>
2024-06-18 19:08:18 +0300

    10.10 to 10.16

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -3,16 +3,22 @@ import Main from './src/components/Main';
 import { StatusBar } from 'expo-status-bar';
 import createApolloClient from './src/utils/apolloClient';
 import { ApolloProvider } from '@apollo/client';
+import AuthStorage from './src/utils/authStorage';
+import AuthStorageContext from './src/contexts/AuthStorageContext';
 
-const apolloClient = createApolloClient();
+
+const authStorage = new AuthStorage()
+const apolloClient = createApolloClient(authStorage);
 
 const App = () => {
   return (
     <>
       <NativeRouter>
         <ApolloProvider client={apolloClient}>
-          <Main />
-          <StatusBar style='auto' />
+          <AuthStorageContext.Provider value={authStorage}>
+            <Main />
+            <StatusBar style='auto' />
+          </AuthStorageContext.Provider>
         </ApolloProvider>
       </NativeRouter>
     </>
--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,7 +1,12 @@
-import { View, StyleSheet, Text, ScrollView } from 'react-native';
+import { View, StyleSheet, Text, ScrollView, Pressable } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
-import { Link } from 'react-router-native';
+import { Link, useNavigate } from 'react-router-native';
+import { useApolloClient, useQuery } from '@apollo/client';
+import { CURRENT_USER } from '../graphql/queries';
+import useAuthStorage from '../hooks/useAuthStorage';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const styles = StyleSheet.create({
   container: {
@@ -20,6 +25,23 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
+  const [user, setUser] = useState(null);
+  const currentUserQuery = useQuery(CURRENT_USER);
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (currentUserQuery.data) {
+      setUser(currentUserQuery.data.me);
+    }
+  }, [currentUserQuery.data]);
+
+  const signOut = async () => {
+    await authStorage.removeAccessToken();
+    await apolloClient.resetStore();
+    navigate('/signIn');
+  };
   return (
     <View style={styles.container}>
       <ScrollView horizontal style={styles.scrollContainer}>
@@ -29,6 +51,15 @@ const AppBar = () => {
         <Link to='/signIn'>
           <Text style={styles.text}>Sign In</Text>
         </Link>
+        {!user ? (
+          <Link to='/signIn'>
+            <Text style={styles.text}>Sign in</Text>
+          </Link>
+        ) : (
+          <Pressable onPress={signOut}>
+            <Text style={styles.text}>Sign out</Text>
+          </Pressable>
+        )}
       </ScrollView>
     </View>
   );
